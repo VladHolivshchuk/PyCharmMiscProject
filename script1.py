f8e20cbfae47802dad500052e955d25a27d27c0c
Vlad <litlelegazp@gmail.com>
2025-03-29 11:59:11 +0200

    Poli

--- script1.py
+++ script1.py
@@ -24,20 +24,59 @@
 # speak(a2)
 # speak(a3)
 
-class Pay:
-    def process(self,money):
-        pass
-class Credit(Pay):
-    def process(self, money):
-        return 'Оплата ' + str(money)+ " грн карткою"
-class Cash(Pay):
-    def process(self, money):
-        return "Оплата " + str(money)+ " грн готівкою"
-class System(Pay):
-    def process(self, money):
-        return "Оплата "+str(money)+" грн онлайн"
+# class Pay:
+#     def process(self,money):
+#         pass
+# class Credit(Pay):
+#     def process(self, money):
+#         return 'Оплата ' + str(money)+ " грн карткою"
+# class Cash(Pay):
+#     def process(self, money):
+#         return "Оплата " + str(money)+ " грн готівкою"
+# class System(Pay):
+#     def process(self, money):
+#         return "Оплата "+str(money)+" грн онлайн"
+#
+# buy=(Credit(),Cash(),System())
+# for i in buy:
+#     print(i.process(123))
+# print()
 
-buy=(Credit(),Cash(),System())
-for i in buy:
-    print(i.process(123))
-print()
+# import random as r
+#
+# class Character:
+#     def __init__(self, name, health):
+#         self.__name = name
+#         self.__health = r.randint(0, 100)
+#
+#     def info_name(self):
+#         return self.__name
+#
+#     def info_hp(self):
+#         return self.__health
+#
+#     def attack(self):
+#         pass
+#
+#     def info_hp(self):
+#         return self.__health
+#
+#     def attack(self):
+#         pass
+#
+#     def take_damage(self, damage):
+#         self.__health-=r.randint(0, 10)
+#
+#     def is_alive(self):
+#         return self.__health>0
+#     def __str__(self):
+#         return self.__name+"my hp:"+str(self.__health)
+#
+# class Warrior(Character):
+#     def __init__(self, name, health):
+#         super().__init__(name, health)
+#
+#     def attack(self,num):
+#         print(self.__name + "attacking knight!")
+#         num.take_damage(r.randint(1, 6))
+
